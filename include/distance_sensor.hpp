0be75838598271ba95f89b322fc30649ed165635
babji <valiya.babji.sk@gmail.com>
2026-09-07 12:27:39 +0530

    feat(thread-lifetime): implement thread life time

--- include/distance_sensor.hpp
+++ include/distance_sensor.hpp
@@ -1,6 +1,6 @@
 #pragma once
-
 #include <atomic>
+#include <thread>
 
 class DistanceSensor
 {
@@ -8,6 +8,11 @@ public:
     void setDistance(double distance);
     double getDistance() const;
 
+    void start();
+    void stop();
+
 private:
-    std::atomic<double> distance{0.0};
+    std::atomic<double> distance{10.0};
+    std::atomic<bool> running{false};
+    std::thread sensorThread;
 };
--- src/distance_sensor.cpp
+++ src/distance_sensor.cpp
@@ -1,4 +1,6 @@
 #include "distance_sensor.hpp"
+#include <thread>
+#include <iostream>
 
 void DistanceSensor::setDistance(double distance)
 {
@@ -8,4 +10,32 @@ void DistanceSensor::setDistance(double distance)
 double DistanceSensor::getDistance() const
 {
     return distance.load();
+}
+
+void DistanceSensor::start()
+{
+    running.store(true);
+    sensorThread = std::thread([this]()
+                               {
+        while (running.load())
+        {
+            setDistance(getDistance() * 2);
+
+            std::cout << "[Sensor] Distance: "
+                    << getDistance()
+                    << std::endl;
+
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(500));
+        } });
+}
+
+void DistanceSensor::stop()
+{
+    running.store(false);
+
+    if (sensorThread.joinable())
+    {
+        sensorThread.join();
+    }
 }
